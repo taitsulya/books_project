2aa88477cb84a2df1acf9c073b61543c4000abb8
taitsulya <iojibka_2000@mail.ru>
2021-09-07 22:37:06 +0300

    Add validations for author_book

--- app/models/author_book.rb
+++ app/models/author_book.rb
@@ -3,4 +3,7 @@
 class AuthorBook < ApplicationRecord
   belongs_to :author
   belongs_to :book
+
+  validates :author_id, uniqueness: { scope: :book_id }
+  validates :author_id, :book_id, presence: true
 end
